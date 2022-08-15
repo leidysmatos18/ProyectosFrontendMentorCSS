25c4dd0e77f98c9c13920ff0d7fff21badb00681
Leidys Matos <matosleidys@gmail.com>
2022-08-15 13:31:54 -0400

    culminacion del formulario con validaciones y cambios de estilos

--- intro-component-with-signup-form-master/main1.js
+++ intro-component-with-signup-form-master/main1.js
@@ -1,44 +1,83 @@
 window.addEventListener("load", () => {
   const form = document.getElementById("formulario")
   const firstName = document.getElementById("name");
-  const lastName = document.getElementById("last-name");
+  const lastname = document.getElementById("lastname");
   const addressEmail = document.getElementById("email");
   const passwordSecret = document.getElementById("password");
 
   form.addEventListener("submit", (e) => {
     e.preventDefault()
 
-    validaCampos()
+    validarCampos()
   })
 
-  const validaCampos = () => {
+  const validarCampos = () => {
     const firstNameValor = firstName.value.trim();
-    const lastNameValor = lastName.value.trim();
+    const lastnameValor = lastname.value.trim();
     const addressEmailValor = addressEmail.value.trim();
     const passwordSecretValor = passwordSecret.value.trim();
+
+    const textoSinNumero = /^[A-Za-z]+$/;
+    const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
     
-    if(!firstNameValor) {
-      console.log("campo vacio ")
-      validaFalla(firstNameValor, "First Name cannot be empty")
+    //validacion del nombre
+    if (!firstNameValor) {
+      validarError(firstName, "First Name cannot be empty")
+    } else if (firstNameValor.length < 3) {
+      validarError(firstName, "First Name cannot be less than 3 characters")
+    } else if (!textoSinNumero.test(firstNameValor)) {
+      validarError(firstName, "First Name cannot have any numbers")
+    } else {
+      validacionExitosa(firstName)
+    }
+
+    //validacion del apellido
+    if (!lastnameValor) {
+      validarError(lastname, "Last name cannot be empty")
+    } else if (!textoSinNumero.test(lastnameValor)) {
+      validarError(lastname, "Last Name cannot have any numbers")
+    } else {
+      validacionExitosa(lastname)
+    }
+
+    //validacion del Correo
+    if(!addressEmailValor) {
+      validarError(addressEmail, "Looks like this is not an Email")
+    } else if(!regexEmail.test(addressEmailValor)) {
+      validarError(addressEmail, "Looks like this is not an Email"); 
     }else {
-      validaOK(firstNameValor)
+      validacionExitosa(addressEmail)
+    }
+
+    //validacion de la contraseña
+    if(!passwordSecretValor) {
+      validarError(passwordSecret, "Password cannot be empty")
+    } else if(passwordSecretValor.length < 8) {
+      validarError(passwordSecret, "Password cannot be less than 8 characters")
+    }else {
+      validacionExitosa(passwordSecret)
     }
   }
-  const validaFalla = (input, msje) => {
-    const inputContainer = input.parentElement
-    console.log(inputContainer)
-    const aviso = document.querySelector(".messageName")
-    const imgAviso = document.querySelectorAll(".alerta")
+  
+  const validarError = (input, mensaje) => {
+    const aviso = input.nextElementSibling.nextElementSibling;
+      aviso.innerText = mensaje
+      aviso.classList.replace("spanAlert", "spanAlertOff") 
 
-    aviso.innerText = msje
-    aviso.classList.replace("spanAlert, spanAlertOff") 
-
-    imgAviso.classList.replace("alerta, alertaOff")
+    const imgAviso = input.nextElementSibling
+      imgAviso.classList.replace("alerta", "alertaOff");
+      
+    input.classList.replace("inputs", "inputAlert")
   }
 
-  const validaOK = (input, msje) => {
-    const inputContainer = input.parentElement
-    console.log(inputContainer)
-    inputContainer.classList.add("inputs")
+  const validacionExitosa = (input, mensaje) => {
+    const aviso = input.nextElementSibling.nextElementSibling;
+      aviso.innerText = mensaje
+      aviso.classList.replace("spanAlertOff", "spanAlert") 
+
+    const imgAviso = input.nextElementSibling
+      imgAviso.classList.replace("alertaOff", "alerta")
+
+    input.classList.replace("inputAlert", "inputs")
   }
 })
